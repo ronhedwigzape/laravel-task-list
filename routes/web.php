b8a5717191f5acb04fe2e45d1b3fbf41b7a89345
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-08-18 10:12:58 +0800

    feat: add sample routes
    Signed-off-by: Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('world');
+
+Route::get('/halo', function () {
+    return redirect()->route('helliwordl');
+});
+
+Route::fallback(function () {
+   return "Still got somewhere";
 });
