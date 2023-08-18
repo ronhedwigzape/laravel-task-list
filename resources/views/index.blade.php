864f8482b5f56a3f7343d401a59d31df2cba920a
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-08-18 11:50:11 +0800

    feat(layouts): add app layout
    Signed-off-by: Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,15 @@
-<h1>
-    The lists of tasks
-</h1>
-<div>
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
 {{--    @if(count($tasks))--}}
-        @forelse($tasks as $task)
-            <div>
-                <a href="{{ route('tasks.show', ['id' => $task->id] ) }}">{{ $task->title }}</a>
-            </div>
-        @empty
-            <div>There are no tasks!</div>
-        @endforelse
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id] ) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
 {{--    @endif--}}
-</div>
+@endsection
