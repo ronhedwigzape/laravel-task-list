1fd5df1244fa04d551f67a46f619c4a48ecab9e9
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-20 14:37:54 +0800

    feat: add a delete button

--- routes/web.php
+++ routes/web.php
@@ -42,12 +42,6 @@ Route::get('/tasks/{task}', function (Task $task) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-//    $data = ;
-//    $task = new Task;
-//    $task->title = $data['title'];
-//    $task->description = $data['description'];
-//    $task->long_description = $data['long_description'];
-//    $task->save();
 
     $task = Task::create($request->validated());
 
@@ -56,18 +50,18 @@ Route::post('/tasks', function (TaskRequest $request) {
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-//    $data = ;
-//    $task->title = $data['title'];
-//    $task->description = $data['description'];
-//    $task->long_description = $data['long_description'];
-//    $task->save();
 
     $task->update($request->validated());
     return redirect()->route('tasks.show', ['task' => $task->id ])
         ->with('success', 'Task updated successfully');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
 
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return "Still got somewhere";
