46a6b00f4604280bb15b326591a1376d35a076c0
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-20 11:23:01 +0800

    feat: add error message
    Signed-off-by: Ron Hedwig Zape <ronzape@ronhedwigzape.com>

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Add Task')
 
 @section('content')
+    {{ $errors }}
     <form method="POST" action="{{ route('tasks.store') }}">
         @csrf {{--  need to add this for every form --}}
         <div>
