5f079bcb831c6388e8e86d4e97661fd393389694
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-20 11:40:05 +0800

    feat: add sessions, errors, and flash message
    Signed-off-by: Ron Hedwig Zape <ronzape@ronhedwigzape.com>

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,23 +2,40 @@
 
 @section('title', 'Add Task')
 
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+        }
+    </style>
+
+@endsection
+
 @section('content')
-    {{ $errors }}
     <form method="POST" action="{{ route('tasks.store') }}">
         @csrf {{--  need to add this for every form --}}
         <div>
             <label for="title">Title</label>
             <input name="title" id="title">
+            @error('title')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="description">Description</label>
             <textarea name="description" id="description" rows="5"></textarea>
+            @error('description')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="long_description">Long Description</label>
             <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('long_description')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task List App</title>
+    @yield('styles')
 </head>
 <body>
     <h1>@yield('title')</h1>
     <div>
+        @if(session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+
         @yield('content')
     </div>
 </body>
