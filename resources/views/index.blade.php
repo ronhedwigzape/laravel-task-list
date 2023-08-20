fc1f028b02df9bd76faff42b4594ea3ff2e0087b
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-20 19:15:42 +0800

    feat: add tailwindcss
    Signed-off-by: Ron Hedwig Zape <ronzape@ronhedwigzape.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,26 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-{{--    @if(count($tasks))--}}
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+            class="font-medium text-gray-700 underline decoration-pink-500"
+        >Add Task!</a>
+    </nav>
+
     @forelse($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['task' => $task->id] ) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task->id] ) }}"
+                @class(['font-bold', 'line-through' => $task->completed ])
+            >{{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks!</div>
     @endforelse
-{{--    @endif--}}
+
     @if($tasks->count())
-        {{ $tasks->links() }}
+        <nav class="mt-4">
+            {{ $tasks->links() }}
+        </nav>
     @endif
+
 @endsection
